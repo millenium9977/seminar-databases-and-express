38f34169fd34a8be6d048b7d4813a17d51f70b6b
Millenium9977 <38467570+millenium9977@users.noreply.github.com>
2020-01-16 19:24:20 +0100

    Todo

--- my-movie-db-backend/server/index.ts
+++ my-movie-db-backend/server/index.ts
@@ -2,6 +2,7 @@ import './common/env';
 import Server from './common/server';
 import routes from './routes';
 
+//TODO: Have to add a bootstrap for the dependency injection
 
 const port = parseInt(process.env.PORT);
 export default new Server()
