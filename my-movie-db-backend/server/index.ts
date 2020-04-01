ace82df2e7c02c4c0832a4b23bf7902e2d4fc1e8
Millenium <38467570+millenium9977@users.noreply.github.com>
2020-04-01 17:09:24 +0200

    added infinite retry to startup

--- my-movie-db-backend/server/index.ts
+++ my-movie-db-backend/server/index.ts
@@ -5,22 +5,33 @@ import logger from './common/logger';
 
 //TODO: Have to add a bootstrap for the dependency injection
 
-export const DEFAULT_ENTRY_SIZE= parseInt(process.env.DEFAULT_ENTRY_SIZE);
+export const DEFAULT_ENTRY_SIZE = parseInt(process.env.DEFAULT_ENTRY_SIZE);
 
-const port        = parseInt(process.env.PORT);
-const server      = new Server();
-const initPromise = server.Setup();
+const port    = parseInt(process.env.PORT);
+const server  = new Server();
 const timeout = parseInt(process.env.TIMEOUT);
 
 logger.debug(`${port}`);
 logger.debug(process.env.MARIA_USER);
 logger.debug(process.env.MARIA_PASSWORD);
 logger.debug(process.env.MARIA_PORT);
+start();
 
-initPromise.then(
-    (server) => {
-        server.router(routes)
-            .listen(port);
-        server.configure(timeout);
+function start() {
+    const initPromise = server.Setup();
+    initPromise.then(
+        (server) => {
+            server.router(routes)
+                .listen(port);
+            server.configure(timeout);
+        });
+    initPromise.catch((err) => {
+        logger.error(err);
+        this.sleep(1000);
+        start();
     });
-initPromise.catch((err) => logger.error(err));
+}
+
+function afterStart() {
+
+}
