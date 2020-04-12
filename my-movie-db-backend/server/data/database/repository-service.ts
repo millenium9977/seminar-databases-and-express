24f03bff84bcbbf90000a036868c2e0037abeb77
Kea Nu <kestue7@gmail.com>
2020-04-12 19:49:36 +0200

    only save and reset via save endpoint

--- my-movie-db-backend/server/data/database/repository-service.ts
+++ my-movie-db-backend/server/data/database/repository-service.ts
@@ -46,15 +46,13 @@ export class RepositoryService {
     }
 
     public async ResetDatabaseWithoutRelations(count: number = DEFAULT_ENTRY_SIZE): Promise<Boolean> {
-        if(!this.Dirty) {
+        if(this.Dirty) {
             return true;
         }
 
         try {
             await this.ResetDatabase();
             await this._csvLoaderManager.WithoutRelations(count);
-
-            this.Dirty = false;
 
             return true;
         } catch (err) {
@@ -64,15 +62,13 @@ export class RepositoryService {
     }
 
     public async ResetDatabaseWithRelations(count: number = DEFAULT_ENTRY_SIZE): Promise<Boolean> {
-        if(!this.Dirty) {
+        if(this.Dirty) {
             return true;
         }
 
         try {
             await this.ResetDatabase();
             await this._csvLoaderManager.WithRelations(count);
-
-            this.Dirty = false;
 
             return true;
         } catch (err) {
